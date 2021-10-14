da43e3112be4dd63c591931c351adbdc15afb53b
Javan5 <vtf9755@gmail.com>
2021-10-14 12:44:05 +0300

    Initial commit

---
+++ src/main/java/com/example/Thanks/Bonus/ThanksBonusApplication.java
@@ -0,0 +1,13 @@
+package com.example.Thanks.Bonus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanksBonusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanksBonusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Thanks/Bonus/ThanksBonusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Thanks.Bonus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanksBonusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
