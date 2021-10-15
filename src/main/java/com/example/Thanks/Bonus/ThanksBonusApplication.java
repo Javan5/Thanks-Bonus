663e6d3ddcc349b5df68b6a814d4f780faf2e8a8
Javan5 <vtf9755@gmail.com>
2021-10-15 11:53:58 +0300

    11:53 15.10.21 Мягко говоря, не работает

--- src/main/java/com/example/Thanks/Bonus/ThanksBonusApplication.java
+++ src/main/java/com/example/Thanks/Bonus/ThanksBonusApplication.java
@@ -1,10 +1,12 @@
 package com.example.Thanks.Bonus;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
+@SpringBootApplication//(exclude = {DataSourceAutoConfiguration.class })
 public class ThanksBonusApplication {
 
 	public static void main(String[] args) {
