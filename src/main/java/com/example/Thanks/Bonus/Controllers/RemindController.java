e439bde8655566ddfd28153bc31ddf12734cb1f8
Javan5 <vtf9755@gmail.com>
2021-10-18 17:17:19 +0300

    Server v1.1 Start

--- src/main/java/com/example/Thanks/Bonus/Controllers/RemindController.java
+++ src/main/java/com/example/Thanks/Bonus/Controllers/RemindController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/")
 @Controller
-@RequestMapping("/index")
 public class RemindController {
 
-	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
 	public String getRemind(ModelMap model){
 		return "Remind";
